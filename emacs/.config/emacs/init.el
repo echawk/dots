efed4363d4367068824dbb13b731b26f39e57350
Ethan <ethan.hawk@valpo.edu>
2021-11-05 21:13:01 -0500

    emacs: add

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,61 @@
+;(load-theme 'leuven)
+(load-theme 'wombat)
+
+; Cleanup the inital screen
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1) ; Disable the scrollbar
+(set-fringe-mode 10) ; give some breathing room
+(tool-bar-mode -1)   ; Disable the toolbar
+(menu-bar-mode -1)   ; Disable the menubar
+(tooltip-mode -1)    ; Disable tooltips
+
+; basic functionality
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+; package stuff
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;(use-package rainbow-delimiters
+;  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
