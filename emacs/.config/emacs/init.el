db05bc41e8932b5a526772bc274b9d2c55522f86
Ethan <ethan.hawk@valpo.edu>
2021-11-10 14:15:16 -0600

    emacs: upd

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,3 +1,9 @@
+; Increases Garbage Collection During Startup
+(setq startup/gc-cons-threshold gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+(defun startup/reset-gc () (setq gc-cons-threshold startup/gc-cons-threshold))
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
 ;(load-theme 'leuven)
 (load-theme 'wombat)
 
