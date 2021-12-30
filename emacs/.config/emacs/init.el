dae4519b18d0d6070d030d7a5cfdeb60e3ec75e5
Ethan <ethan.hawk@valpo.edu>
2021-12-29 18:21:26 -0600

    emacs: add imaxima integration

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -66,3 +66,15 @@
   (setq evil-want-C-u-scroll t)
   :config
   (evil-mode 1))
+
+; Only init imaxima if the elisp files exist.
+(when (file-exists-p "/usr/share/emacs/site-lisp/maxima")
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/maxima")
+  (autoload 'maxima-mode "maxima" "Maxima mode" t)
+  (autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+  (autoload 'maxima "maxima" "Maxima interaction" t)
+  (autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+  (setq imaxima-use-maxima-mode-flag t)
+  ; Sets the font for the LaTeX output.
+  (setq imaxima-fnt-size "LARGE")
+  (add-to-list 'auto-mode-alist '("\\.ma[cx]\\'" . maxima-mode)))
