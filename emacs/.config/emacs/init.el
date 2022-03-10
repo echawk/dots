0ae72b75fbcbba01d02787c37d90e1cef1ea7f39
Ethan <ethan.hawk@valpo.edu>
2022-03-10 01:13:56 -0600

    emacs: add shell & python mode to tree sitter

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -147,6 +147,8 @@
 (use-package tree-sitter
   :defer
   :hook ((go-mode . tree-sitter-hl-mode)
+         (shell-mode . tree-sitter-hl-mode)
+         (python-mode . tree-sitter-hl-mode)
          (caml-mode . tree-sitter-hl-mode)))
 (use-package tree-sitter-langs :after tree-sitter)
 
