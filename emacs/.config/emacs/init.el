9ebd3ef2c90fab4416a81bc7e8d63a47cb09b85e
Ethan <ethan.hawk@valpo.edu>
2022-03-10 23:29:03 -0600

    emacs: fix

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -75,7 +75,7 @@
   (let ((bootstrap-file
           (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
         (bootstrap-version 5))
-    (unless (file-existsp bootstrap-file)
+    (unless (file-exists-p bootstrap-file)
       (with-current-buffer
         (url-retrieve-synchronously
           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
