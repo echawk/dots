448287a77403a1c2742d822325628c50db98ec74
Ethan <ethan.hawk@valpo.edu>
2022-03-10 19:33:16 -0600

    emacs: add bootstrap functions for both package & straight

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -58,13 +58,38 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t)
+
+(defun package-bootstrap ()
+  "Bootstrap package.el"
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(defun straight-bootstrap ()
+  "Bootstrap straight.el"
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+          (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 5))
+    (unless (file-existsp bootstrap-file)
+      (with-current-buffer
+        (url-retrieve-synchronously
+          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+          'slient 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+  (straight-use-package 'use-package)
+  (setq straight-use-package-by-default t))
+
+(setq use-straight nil)
+(if use-straight
+  (straight-bootstrap)
+  (package-bootstrap))
 
 (use-package ivy
   :diminish
