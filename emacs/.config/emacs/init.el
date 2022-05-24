355b42db4ee52f745bb7d040eec5bebadea5432f
Ethan <ethan.hawk@valpo.edu>
2022-05-24 15:28:41 -0500

    emacs: drop ivy - use vertico, marginalia & orderless instead
    also remove ':defer' from which-key

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -86,26 +86,20 @@
 
 (use-package async :defer)
 
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+(use-package vertico
+  :init
+  (vertico-mode))
+
+(use-package marginalia
+  :init
+  (marginalia-mode))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package which-key
-  :defer
   :config
   (which-key-mode))
 
