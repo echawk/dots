328ef50999e9267089b2bf14d8e16e26b99560c5
Ethan <ethan.hawk@valpo.edu>
2022-11-01 15:05:30 -0500

    emacs: switch theme to modus-operandi

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,5 +1,5 @@
 ;; (load-theme 'tango)
-(load-theme 'leuven)
+(load-theme 'modus-operandi)
 
 (setq column-number-mode t) ; Show column number too
 (fset 'yes-or-no-p 'y-or-n-p) ; Don't ask to spell out 'yes'
