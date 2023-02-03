8b36441732fcf2b30c77c53941b5772981e582d1
Ethan <ethan.hawk@valpo.edu>
2023-02-02 22:39:29 -0600

    emacs: init.el ~ remove 'load-theme'

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,6 +1,3 @@
-(load-theme 'modus-operandi)
-
-;; package stuff
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org"   . "https://orgmode.org/elpa/")
