241439bd593c9f510418b376511b615df753f0e0
Ethan <ethan.hawk@valpo.edu>
2023-02-10 15:22:28 -0600

    emacs: early-init.el: nits

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,25 +1,25 @@
-; Increases Garbage Collection During Startup
+;; Increases Garbage Collection During Startup
 (setq startup/gc-cons-threshold gc-cons-threshold)
 (setq gc-cons-threshold most-positive-fixnum)
 (defun startup/reset-gc () (setq gc-cons-threshold startup/gc-cons-threshold))
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
 
-; Cleanup the inital screen
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1) ; Disable the scrollbar
-(set-fringe-mode 10) ; give some breathing room
-(tool-bar-mode -1)   ; Disable the toolbar
-(menu-bar-mode -1)   ; Disable the menubar
-(tooltip-mode -1)    ; Disable tooltips
+;; Cleanup the inital screen
+(setq inhibit-startup-message t
+      inhibit-startup-screen t
+      frame-inhibit-implied-resize t
+      desktop-resore-forces-onscreen nil)
+(scroll-bar-mode -1)
+(set-fringe-mode 10)
+(tool-bar-mode -1)
+;;(tooltip-mode -1)
 (global-display-line-numbers-mode 1)  ; Show line numbers
-(setq frame-inhibit-implied-resize t)
-(setq desktop-resore-forces-onscreen nil)
 (advice-add #'x-apply-session-resources :override #'ignore) ; Ignore Xresources
-(setq straight-check-for-modifications nil)
-;(setq straight-vc-git-default-clone-depth 1)
 
-; https://reddit.com/r/emacs/comments/np6ey4/how_packageel_works_with_use_package/
-(setq package-enable-at-startup nil)
+;; https://reddit.com/r/emacs/comments/np6ey4/how_packageel_works_with_use_package/
+(setq straight-check-for-modifications nil
+      straight-vc-git-default-clone-depth 1
+      package-enable-at-startup nil)
 
 (set-face-attribute 'default nil :height 140)
 ;; Set the theme based on the time.
