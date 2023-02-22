17a72a88d1ac15e67e611e8046dfc5c7af7787b3
Ethan <ethan.hawk@valpo.edu>
2023-02-22 12:02:24 -0600

    emacs: early-init.el ~ add performance code from DOOM

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -22,6 +22,54 @@
       package-enable-at-startup nil)
 
 (set-face-attribute 'default nil :height 140)
+
+;; https://github.com/doomemacs/doomemacs/blob/master/lisp/doom.el#L314
+;; Below is taken straight from DOOM. Time will tell if I'll keep it.
+(unless (daemonp)
+  ;; PERF: `file-name-handler-alist' is consulted on each call to `require',
+  ;;   `load', or various file/io functions (like `expand-file-name' or
+  ;;   `file-remote-p'). You get a noteable boost to startup time by unsetting
+  ;;   or simplifying its value.
+  (let ((old-value (default-toplevel-value 'file-name-handler-alist)))
+    (setq file-name-handler-alist
+          ;; HACK: If the bundled elisp for this Emacs install isn't
+          ;;   byte-compiled (but is compressed), then leave the gzip file
+          ;;   handler there so Emacs won't forget how to read read them.
+          ;;
+          ;;   calc-loaddefs.el is our heuristic for this because it is built-in
+          ;;   to all supported versions of Emacs, and calc.el explicitly loads
+          ;;   it uncompiled. This ensures that the only other, possible
+          ;;   fallback would be calc-loaddefs.el.gz.
+          (if (eval-when-compile
+                (locate-file-internal "calc-loaddefs.el" load-path))
+              nil
+            (list (rassq 'jka-compr-handler old-value))))
+    ;; Make sure the new value survives any current let-binding.
+    (set-default-toplevel-value 'file-name-handler-alist file-name-handler-alist)
+    ;; Remember it so it can be reset where needed.
+    (put 'file-name-handler-alist 'initial-value old-value)))
+
+;; (unless noninteractive
+;;   ;; PERF: Resizing the Emacs frame (to accommodate fonts that are smaller or
+;;   ;;   larger than the system font) appears to impact startup time
+;;   ;;   dramatically. The larger the delta in font size, the greater the delay.
+;;   ;;   Even trivial deltas can yield a ~1000ms loss, though it varies wildly
+;;   ;;   depending on font size.
+;;   (setq frame-inhibit-implied-resize t)
+
+;;   ;; PERF,UX: Reduce *Message* noise at startup. An empty scratch buffer (or
+;;   ;;   the dashboard) is more than enough, and faster to display.
+;;   (setq inhibit-startup-screen t
+;;         inhibit-startup-echo-area-message user-login-name)
+;;   ;; PERF,UX: Remove "For information about GNU Emacs..." message at startup.
+;;   ;;   It's redundant with our dashboard and incurs a premature redraw.
+;;   (advice-add #'display-startup-echo-area-message :override #'ignore)
+;;   ;; PERF: Suppress the vanilla startup screen completely. We've disabled it
+;;   ;;   with `inhibit-startup-screen', but it would still initialize anyway.
+;;   ;;   This involves some file IO and/or bitmap work (depending on the frame
+;;   ;;   type) that we can no-op for a free 50-100ms boost in startup time.
+;;   (advice-add #'display-startup-screen :override #'ignore))
+
 ;; Set the theme based on the time.
 ;; If it's before 6AM or after 8PM, switch to vivendi.
 (let ((curr-hour (string-to-number
