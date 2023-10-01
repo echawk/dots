3adcfb13023de57862408044afdd9b7460d8e56c
Ethan <ethan.hawk@valpo.edu>
2023-10-01 02:03:36 -0500

    shell: more xdg cleanup

--- shell/.config/shell/env.sh
+++ shell/.config/shell/env.sh
@@ -78,6 +78,10 @@ export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
 export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
 export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
 
+export SOLARGRAPH_CACHE="$XDG_CACHE_HOME/solargraph"
+
+export NIMBLE_DIR="$XDG_DATA_HOME/nimble"
+
 # ~/ cleanup
 export LESS=-R
 export LESSHISTFILE="-"
